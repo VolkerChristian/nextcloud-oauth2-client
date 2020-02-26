8f854022cd93526d33b61d2b76252aa118c4c51d
Volker Christian <volker.christian@fh-hagenberg.at>
2020-02-26 13:09:22 +0100

    Update

--- src/NextcloudUserRepository.ts
+++ src/NextcloudUserRepository.ts
@@ -5,35 +5,35 @@ import { NextcloudToken } from './entity/NextcloudToken';
 @EntityRepository(NextcloudUser)
 export class NextcloudUserRepository extends Repository<NextcloudUser> {
     getUser(userName: string) {
-        return new Promise<NextcloudUser>((resolve, reject) => {
-            this.createQueryBuilder('user')
-                .leftJoinAndMapOne(
-                    'user.token',
-                    NextcloudToken,
-                    'token',
-                    'token.userId = user.id'
-                )
-                .where(
-                    'user.userName = :userName', { userName: userName }
-                )
-                .getOne()
-                .then(user => resolve(user))
-                .catch(reason => reject(reason))
-        });
-    }
+//        return new Promise<NextcloudUser>((resolve, reject) => {
+        return this.createQueryBuilder('user')
+            .leftJoinAndMapOne(
+                'user.token',
+                NextcloudToken,
+                'token',
+                'token.userId = user.id'
+            )
+            .where(
+                'user.userName = :userName', { userName: userName }
+            )
+            .getOne();
+//                .then(user => resolve(user))
+//                .catch(reason => reject(reason))
+        }//);
+//    }
 
     getAllUser() {
-        return new Promise<NextcloudUser[]>((resolve, reject) => {
-            this.createQueryBuilder('user')
-                .leftJoinAndMapOne(
-                    'user.token',
-                    NextcloudToken,
-                    'token',
-                    'token.userId = user.id'
-                )
-                .getMany()
-                .then(user => resolve(user))
-                .catch(reason => reject(reason))
-        });
-    }
+//        return new Promise<NextcloudUser[]>((resolve, reject) => {
+        return this.createQueryBuilder('user')
+            .leftJoinAndMapOne(
+                'user.token',
+                NextcloudToken,
+                'token',
+                'token.userId = user.id'
+            )
+            .getMany();
+//                .then(user => resolve(user))
+//                .catch(reason => reject(reason))
+        }//);
+//    }
 }
